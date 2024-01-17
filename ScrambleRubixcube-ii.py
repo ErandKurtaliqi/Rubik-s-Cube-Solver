fccb7497a1a1ed1e28b05329a492a4c46d8b60cd
Vlera Luma <118343087+VleraLuma@users.noreply.github.com>
2024-01-17 21:56:50 +0100

    Update ScrambleRubixcube-ii.py

--- ScrambleRubixcube-ii.py
+++ ScrambleRubixcube-ii.py
@@ -10,3 +10,27 @@ def RightCW(x):  # action 9 Front right clock wise
     x[15:18, 2] = np.fliplr([temp2])[0]
     x[6:9, 2] = temp3
 
+def RightACW(x):  # action 10
+    RightCW(x)
+    RightCW(x)
+    RightCW(x)
+
+def BackCW(x):  # action 11 Front  back clock wise
+
+    x[12:15, :] = np.fliplr(x[12:15, :].transpose())
+    temp1 = np.array(x[0, 0:3])
+    temp2 = np.array(x[3:6, 0])
+    temp3 = np.array(x[17, 0:3])
+    temp4 = np.array(x[9:12, 2])
+    x[0, 0:3] = temp4
+    x[3:6, 0] = np.fliplr([temp1])[0]
+    x[17, 0:3] = temp2
+    x[9:12, 2] = np.fliplr([temp3])[0]
+
+
+def BackACW(x):  # action 12
+    BackCW(x)
+    BackCW(x)
+    BackCW(x)
+
+
